ef761855e9dfe0a3a9390018f7d71a8902c66eee
vitorcontiz <vitorcontizgit@gmail.com>
2024-06-07 13:55:08 -0300

    feat: ensure DbRetriveallVehicle throw if RetrriveAllVehicleRepository throws

--- tests/data/usecases/db-retrive-all-vehicle.spec.ts
+++ tests/data/usecases/db-retrive-all-vehicle.spec.ts
@@ -1,23 +1,36 @@
 import { DbRetriveAllVehicle } from '@/data/usecases/db-retrive-all-vehicle'
 import { RetriveAllVehicleRespositorySpy } from '@/tests/data/mocks/mock-db-vehicle'
+import { throwError } from '@/tests/domain/mocks/test-helpers'
 
-
+type SutTypes = {
+  sut: DbRetriveAllVehicle
+  retriveAllVehicleRepositorySpy: RetriveAllVehicleRespositorySpy
+}
+const makeSut = (): SutTypes => {
+  const retriveAllVehicleRepositorySpy = new RetriveAllVehicleRespositorySpy()
+  const sut = new DbRetriveAllVehicle(retriveAllVehicleRepositorySpy)
+  return {
+    sut, retriveAllVehicleRepositorySpy
+  }
+}
 describe('DbRetriveAllVehicle usecase', () => {
-  type SutTypes = {
-    sut: DbRetriveAllVehicle
-    retriveAllVehicleRepositorySpy: RetriveAllVehicleRespositorySpy
-  }
-  const makeSut = (): SutTypes => {
-    const retriveAllVehicleRepositorySpy = new RetriveAllVehicleRespositorySpy()
-    const sut = new DbRetriveAllVehicle(retriveAllVehicleRepositorySpy)
-    return {
-      sut, retriveAllVehicleRepositorySpy
-    }
-  }
+
   test('Should call RetriveAllVehicleRepository ', () => {
     const { sut, retriveAllVehicleRepositorySpy } = makeSut()
     const retriveSpy = jest.spyOn(retriveAllVehicleRepositorySpy, 'retrive')
     sut.retriveAll()
     expect(retriveSpy).toHaveBeenCalled()
   })
+  test('Should throw if RetriveAllVehicleRepository throws', async () => {
+    try {
+      const { sut, retriveAllVehicleRepositorySpy } = makeSut()
+      jest.spyOn(retriveAllVehicleRepositorySpy, 'retrive').mockImplementationOnce(throwError)
+      const promise = sut.retriveAll()
+      await expect(promise).rejects.toThrow()
+    } catch (error) {
+
+    }
+
+  })
+
 })
